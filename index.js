d0c538185cc77e5486803ca5859316e4bf82e087
Ariff123 <76693708+Ariff123@users.noreply.github.com>
2021-01-12 17:13:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ client.on('group-participants-update', async (anu) => {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["6285788555068@s.whatsapp.net"] 
+			const ownerNumber = ["6285376980664@s.whatsapp.net"] 
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
